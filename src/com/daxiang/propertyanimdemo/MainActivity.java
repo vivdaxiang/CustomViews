505a2c994691d25760bc3291c80382317cd8ab52
daxiang <bmd1989@163.com>
2016-07-08 17:08:28 +0800

    修改包结构

---
+++ src/com/daxiang/propertyanimdemo/MainActivity.java
@@ -0,0 +1,64 @@
+package com.daxiang.propertyanimdemo;
+
+import com.daxiang.propertyanimdemo.animation.CustomEvaluator;
+import com.daxiang.propertyanimdemo.animation.RotationActivity;
+import com.daxiang.propertyanimdemo.animation.StickyAnimActivity;
+import com.daxiang.propertyanimdemo.animation.SystemEvaluator;
+import com.daxiang.propertyanimdemo.animation.TranslateActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends Activity implements OnClickListener {
+
+	private Button rotation;
+	private Button translate;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		initViews();
+	}
+
+	private void initViews() {
+		rotation = (Button) findViewById(R.id.rotation);
+		rotation.setOnClickListener(this);
+		translate = (Button) findViewById(R.id.translate);
+		translate.setOnClickListener(this);
+		findViewById(R.id.systemEvaluator).setOnClickListener(this);
+		findViewById(R.id.customEvaluator).setOnClickListener(this);
+		findViewById(R.id.sticky).setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.rotation:
+			startActivity(new Intent(MainActivity.this, RotationActivity.class));
+			break;
+
+		case R.id.translate:
+			startActivity(new Intent(MainActivity.this, TranslateActivity.class));
+			break;
+
+		case R.id.systemEvaluator:
+			startActivity(new Intent(MainActivity.this, SystemEvaluator.class));
+			break;
+
+		case R.id.customEvaluator:
+			startActivity(new Intent(MainActivity.this, CustomEvaluator.class));
+			break;
+
+		case R.id.sticky:
+			startActivity(new Intent(MainActivity.this, StickyAnimActivity.class));
+			break;
+		}
+	}
+
+}
---
+++ src/com/daxiang/propertyanimdemo/animation/TranslateActivity.java
@@ -0,0 +1,133 @@
+package com.daxiang.propertyanimdemo.animation;
+
+import com.daxiang.propertyanimdemo.R;
+
+import android.animation.Animator;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewPropertyAnimator;
+import android.widget.Button;
+import android.widget.TextView;
+
+/**
+ * Translate动画；
+ *
+ * @author daxiang
+ * @date 2016年6月28日
+ * @time 下午5:01:02
+ */
+public class TranslateActivity extends Activity implements OnClickListener {
+
+	private Button animatingButton;
+	private ViewPropertyAnimator animator;
+
+	private float translationX;
+	private float translationY;
+	// private float translationZ;
+
+	private TextView tv_value;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.activity_translate);
+		initViews();
+	}
+
+	private void initViews() {
+		findViewById(R.id.x).setOnClickListener(this);
+		findViewById(R.id.xBy).setOnClickListener(this);
+		findViewById(R.id.y).setOnClickListener(this);
+		findViewById(R.id.yBy).setOnClickListener(this);
+
+		findViewById(R.id.translationX).setOnClickListener(this);
+		findViewById(R.id.translationXBy).setOnClickListener(this);
+		findViewById(R.id.translationY).setOnClickListener(this);
+		findViewById(R.id.translationYBy).setOnClickListener(this);
+		findViewById(R.id.translationZ).setOnClickListener(this);
+		findViewById(R.id.translationZBy).setOnClickListener(this);
+		findViewById(R.id.goBack).setOnClickListener(this);
+
+		tv_value = (TextView) findViewById(R.id.tv_value);
+
+		animatingButton = (Button) findViewById(R.id.animatingObject);
+		translationX = animatingButton.getTranslationX();
+		translationY = animatingButton.getTranslationY();
+		// translationZ = animatingButton.getTranslationZ();
+	}
+
+	@Override
+	public void onClick(View v) {
+		if (animator == null) {
+			animator = animatingButton.animate().setDuration(2000).setListener(new Animator.AnimatorListener() {
+
+				@Override
+				public void onAnimationStart(Animator animation) {
+
+				}
+
+				@Override
+				public void onAnimationRepeat(Animator animation) {
+
+				}
+
+				@Override
+				public void onAnimationEnd(Animator animation) {
+					tv_value.setText("x = " + animatingButton.getX() + " y = " + animatingButton.getY()
+							+ "\n translationX=" + animatingButton.getTranslationX() + " translationY="
+							+ animatingButton.getTranslationY());
+				}
+
+				@Override
+				public void onAnimationCancel(Animator animation) {
+
+				}
+			});
+		}
+
+		switch (v.getId()) {
+
+		case R.id.x:
+			animator.x(0);// 相对于父控件的x坐标；
+			break;
+
+		case R.id.xBy:
+			animator.xBy(300);
+			break;
+		case R.id.y:
+			animator.y(200);
+			break;
+		case R.id.yBy:
+			animator.yBy(200);
+			break;
+		case R.id.translationX:
+			animator.translationX(300); // 相对于自己左边缘的坐标；
+			break;
+
+		case R.id.translationXBy:
+			animator.translationXBy(-300);
+			break;
+		case R.id.translationY:
+			animator.translationY(200);// 相对于自己顶部的坐标；
+
+			break;
+		case R.id.translationYBy:
+			animator.translationYBy(200);
+
+			break;
+		// case R.id.translationZ:
+		// animator.translationZ(50);
+		// break;
+		// case R.id.translationZBy:
+		// animator.translationZBy(50);
+		// break;
+		case R.id.goBack:
+			animator.translationX(translationX).translationY(translationY);// .translationZ(translationZ);
+			break;
+		}
+	}
+
+}
